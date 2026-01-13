bbc40f2c39fcbb3dcaeacaea0be48af5cdcd753a
moustagesaid <saidmoustage2003@gmail.com>
2026-01-13 17:26:35 +0100

    rani bdit flheader

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Header } from "./header/header";
+import { Hero } from "./hero/hero";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
