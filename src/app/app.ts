2aa0433532e857b1ab31e284ca3cc6a79938553d
moustagesaid <saidmoustage2003@gmail.com>
2026-01-15 16:02:25 +0100

    drt chi l3ibat

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { Header } from "./header/header";
 import { Hero } from "./hero/hero";
+import { Collection } from "./collection/collection";
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Hero],
+  imports: [Header, Hero, Collection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
