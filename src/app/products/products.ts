3a531857957a17a3fb9f66077fc1fd797f006efa
moustagesaid <saidmoustage2003@gmail.com>
2026-02-09 09:59:19 +0100

    update2

--- src/app/products/products.ts
+++ src/app/products/products.ts
@@ -31,8 +31,165 @@ export class Products implements OnInit, OnDestroy {
 
   selectedCategory: 'men' | 'women' | 'couples' = 'women';
 
-  // 4. Start with an empty list (it will fill up from the database)
-  products: Product[] = [];
+  products: Product[] = [
+    // Women's Products
+    {
+      id: 1,
+      name: 'AURUM',
+      subName: 'MIRA',
+      price: 129.99,
+      image: '/assets/mira.png',
+      category: 'women'
+    },
+    {
+      id: 2,
+      name: 'AURUM',
+      subName: 'LYRA',
+      price: 149.99,
+      image: '/assets/lyra.png',
+      category: 'women'
+    },
+    {
+      id: 3,
+      name: 'AURUM',
+      subName: 'MODA',
+      price: 139.99,
+      image: '/assets/moda.png',
+      category: 'women'
+    },
+    {
+      id: 4,
+      name: 'AURUM',
+      subName: 'NOVA',
+      price: 159.99,
+      image: '/assets/nova.png',
+      category: 'women'
+    },
+    {
+      id: 4,
+      name: 'AURUM',
+      subName: 'SOLIS',
+      price: 159.99,
+      image: '/assets/Solis.png',
+      category: 'women'
+    },
+    {
+      id: 4,
+      name: 'OPULENCE',
+      subName: 'Blossom',
+      price: 159.99,
+      image: '/assets/women6.png',
+      category: 'women'
+    },
+    // Men's Products
+    {
+      id: 8,
+      name: 'AURUM',
+      subName: 'Back-end',
+      price: 159.99,
+      image: '/assets/f3.png',
+      category: 'men'
+    },
+    {
+      id: 8,
+      name: 'AURUM',
+      subName: 'Front-end',
+      price: 159.99,
+      image: '/assets/f2.png',
+      category: 'men'
+    },
+    {
+      id: 8,
+      name: 'AURUM',
+      subName: 'Foundation',
+      price: 159.99,
+      image: '/assets/f1.png',
+      category: 'men'
+    },
+    {
+      id: 8,
+      name: 'AURUM',
+      subName: 'Celebration',
+      price: 159.99,
+      image: '/assets/f4.png',
+      category: 'men'
+    },
+    {
+      id: 5,
+      name: 'AURUM',
+      subName: 'AXIS',
+      price: 129.99,
+      image: '/assets/aurum axis.png',
+      category: 'men'
+    },
+    {
+      id: 6,
+      name: 'AURUM',
+      subName: 'VANT',
+      price: 149.99,
+      image: '/assets/aurum vant.png',
+      category: 'men'
+    },
+
+    {
+      id: 7,
+      name: 'AURUM',
+      subName: 'CORE',
+      price: 139.99,
+      image: '/assets/aurum core.png',
+      category: 'men'
+    },
+    {
+      id: 8,
+      name: 'AURUM',
+      subName: 'LUXE',
+      price: 159.99,
+      image: '/assets/aurumluxe.png',
+      category: 'men'
+    },
+    {
+      id: 8,
+      name: 'AURUM',
+      subName: 'Ferrum',
+      price: 159.99,
+      image: '/assets/AURUM Ferrum.png',
+      category: 'men'
+    },
+    {
+      id: 8,
+      name: 'AURUM',
+      subName: 'Titanium',
+      price: 159.99,
+      image: '/assets/AURUM Titanium.png',
+      category: 'men'
+    },
+    {
+      id: 8,
+      name: 'AURUM',
+      subName: 'Cuprum',
+      price: 159.99,
+      image: '/assets/AURUM Cuprum.png',
+      category: 'men'
+    },
+
+    // Packs Products
+    {
+      id: 9,
+      name: 'MEN\'S PACK',
+      subName: 'Essential Collection',
+      price: 459.99,
+      image: '/assets/mpack1.png',
+      category: 'couples'
+    },
+    {
+      id: 10,
+      name: 'WOMEN\'S PACK',
+      subName: 'Luxury Collection',
+      price: 399.99,
+      image: '/assets/wpack1.png',
+      category: 'couples'
+    }
+  ];
 
   constructor(
     private cartService: CartService,
