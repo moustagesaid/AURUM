883885dc477c618a84810d111cb73b3ebb4a7d57
Ussefhn37 <youssefhanin717@gmail.com>
2026-02-10 09:32:11 +0100

    Updates

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css'
 })
